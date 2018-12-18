861ed1a0c1be31ad60586b14569cae784ae9d20d
chensirui <chensirui@rayvision.com>
2018-12-18 16:20:58 +0800

    调试maya_demo_chensr_linux.py

--- demos/maya_demo_chensr_linux.py
+++ demos/maya_demo_chensr_linux.py
@@ -9,7 +9,7 @@ sys.path.append(renderSDK_path)
 from renderSDK.Rayvision import Rayvision
 
 # 1.Log in
-rayvision = Rayvision(domain_name='test.renderbus.com', platform='2', access_id='kz5uwhPULZ2SgosYHL1eJIIBaSgWVkZp', access_key='3a3251ac700db507f806874a68f1fd8a', workspace='/root/chensr/gitlab/log_path')
+rayvision = Rayvision(domain_name='test.renderbus.com', platform='2', access_id='kz5uwhPULZ2SgosYHL1eJIIBaSgWVkZp', access_key='3a3251ac700db507f806874a68f1fd8a', workspace='/root/chensr/workspace')
 
 # 2.Set up rendering environment(plug-in configuration, project name）
 rayvision.set_render_env(cg_name='Maya', cg_version='2016', plugin_config={}, label_name='dasdd')
@@ -21,9 +21,10 @@ scene_info_render, task_info = rayvision.analyse(cg_file=r'/root/chensr/gitlab/r
 error_info_list = rayvision.check_error_warn_info()
 
 # 5.User can modify the parameter list(optional), and then proceed to job submitting
-scene_info_render_new = scene_info_render
-task_info_new = task_info
-rayvision.submit_job(scene_info_render_new, task_info_new)
+task_info['os_name'] = '0'  # Linux
+
+rayvision.submit_job(scene_info_render, task_info)
 
 # 6.Download
-# rayvision.download(job_id_list=[370271], local_dir=r"d:\project\output")
+rayvision.auto_download(job_id_list=[job_id], local_dir=r"/root/chensr/workspace/output")
+# rayvision.auto_download_after_job_completed(job_id_list=[job_id], local_dir=r"/root/chensr/workspace/output")
